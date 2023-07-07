3a43748d394ceab04b1debe57fd289213923a5bc
Md Kowsar Hossain Sakib <mdsakib10098@gmail.com>
2023-07-07 21:50:47 +1000

    Updates

---
+++ precache-manifest.f6d08f0987f32c8421d1f005a45054b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08494ccc7204a8b1ac0b084256bd06d9",
+    "url": "/FairVis/index.html"
+  },
+  {
+    "revision": "40767fb5be963e153790",
+    "url": "/FairVis/static/css/main.6cc91895.chunk.css"
+  },
+  {
+    "revision": "e1dc68c7b38b776592d5",
+    "url": "/FairVis/static/js/2.f5ca92f5.chunk.js"
+  },
+  {
+    "revision": "b4f4f8652cb709f9ab34ba32ef4fa634",
+    "url": "/FairVis/static/js/2.f5ca92f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40767fb5be963e153790",
+    "url": "/FairVis/static/js/main.c03dcfd1.chunk.js"
+  },
+  {
+    "revision": "83e26917ad8a548383db",
+    "url": "/FairVis/static/js/runtime-main.b0a24695.js"
+  },
+  {
+    "revision": "94c06f584ac6c457208154dc55481302",
+    "url": "/FairVis/static/media/adult.94c06f58.csv"
+  },
+  {
+    "revision": "bd26b7d4a2fea1dfb0c61021ea0b847c",
+    "url": "/FairVis/static/media/compas.bd26b7d4.csv"
+  }
+]);
